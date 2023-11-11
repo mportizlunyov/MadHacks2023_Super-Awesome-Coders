35f592daab2d06ddceb752b67234719121530203
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2023-11-11 16:24:43 -0600

    Update timer.js

--- static/timer.js
+++ static/timer.js
@@ -1,2 +1,13 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + Date() + "</h1>"
+function Sec_30_Timer() {
+    alert("Done!");
+}
+function confirmClick() {
+    alert("Clicked!")
+    document.body.innerHTML = "<p>Yes?</p>"
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", confirmClick);
+
+setTimeout(Sec_30_Timer, 30000);
